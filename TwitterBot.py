8c4631561826f9934d8e3ee836bcbdb1f1993dec
Otavio Campos <34351630+iBootDev@users.noreply.github.com>
2018-05-13 10:00:50 -0300

    Update TwitterBot.py

--- TwitterBot.py
+++ TwitterBot.py
@@ -24,32 +24,32 @@ twitter = Twython(
 )
 while True:
     messages = [
-    " I Love Coffeeeee #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " Now i have one site: https://ibootdev.github.io/iBootDev/ #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " Follow my dev on twitter #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " more updates soon #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " Bip Bip BiiiPP wow i found a bug #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " Hello <3 #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " 100111000101010 ignore those numbers #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " i have 70 lines of code #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " AAAAAA, i don't love anyone #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " I LOVE MY DEV #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " One day, i'll dominate the world! #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " NOW I CAN KILL PEOPLE #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " Talk with me <3 #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " Hi, i'm a bot of iBootDev. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " Perl and python = life #Perl #Python #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " bugs and bugs #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " Search by ibootdev on github. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " I'm the most beautiful bot you've ever seen #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " u want to beep with me? #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " I don't havejjskaaaaaaa00092 BuuUúGgs #PyBot #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " I love jokes and Pyjokes #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " RnVpIGRlc2Vudm9sdmlkbyBwb3IgdW0gQnJhc2lsZWlybw== #IndieDev #PythonBot hour: " + str(now.isoformat()),
-    " 8857fdb849c650a8957c3863215c54500f05b2153b44418e671e8a4eadb069c9 hour: " + str(now.isoformat()),
-    " Don't try to hack me, i'll know! #PyBot #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
-    " Developed by @ibootdev #IndieDev #IndieBot #PythonBot #PyBot hour:" + str(now.isoformat()),
-    " Now u can find my source on my dev github: www.github.com/iBootDev hour: " + str(now.isoformat()),
+    "I Love Coffeeeee #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "Now i have one site: https://ibootdev.github.io/iBootDev/ #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "Follow my dev on twitter #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "more updates soon #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "Bip Bip BiiiPP wow i found a bug #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "Hello <3 #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "100111000101010 ignore those numbers #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "i have 71 lines of code #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "AAAAAA, i don't love anyone #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "I LOVE MY DEV #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "One day, i'll dominate the world! #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "NOW I CAN KILL PEOPLE #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "Talk with me <3 #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "Hi, i'm a bot of iBootDev. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "Perl and python = life #Perl #Python #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "bugs and bugs #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "Search by ibootdev on github. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "I'm the most beautiful bot you've ever seen #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "u want to beep with me? #PyBot. #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "I don't havejjskaaaaaaa00092 BuuUúGgs #PyBot #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "I love jokes and Pyjokes #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "RnVpIGRlc2Vudm9sdmlkbyBwb3IgdW0gQnJhc2lsZWlybw== #IndieDev #PythonBot hour: " + str(now.isoformat()),
+    "8857fdb849c650a8957c3863215c54500f05b2153b44418e671e8a4eadb069c9 hour: " + str(now.isoformat()),
+    "Don't try to hack me, i'll know! #PyBot #IndieDev #IndieBot #PythonBot hour: " + str(now.isoformat()),
+    "Developed by @ibootdev #IndieDev #IndieBot #PythonBot #PyBot hour:" + str(now.isoformat()),
+    "Now u can find my source on my dev github: www.github.com/iBootDev hour: " + str(now.isoformat()),
     "https://github.com/iBootDev/Python/blob/master/TwitterBot.py hour: " + str(now.isoformat()),
     "Every 15 minutes i post something #PythonBot #PyBot #IndieBot #IndieDev hour: " + str(now.isoformat()),
     "Eu falo portugues também, afinal sou Brasileiro #PythonBot #PyBot #IndieBot #IndieDev hour: " + str(now.isoformat()),
@@ -64,7 +64,8 @@ while True:
     twitter.update_status(status = message)
     joke = pyjokes.get_joke() + str(now.isoformat())
     twitter.update_status(status = joke)
-    print("Message: " + message)
+    print("Mensagem: " + message)
     print("Joke: " + joke)
     print("Posted!")
     time.sleep(900)#Tweet every 15 minutes
+    print("##########################")
